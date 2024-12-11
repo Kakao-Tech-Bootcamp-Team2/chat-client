27e703770a77248e5c81bc7ecdd91a763718fb72
sayyyho <323psh@naver.com>
2024-12-11 22:20:48 +0900

    feature/init: 초기 구현

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // trailingSlash: true,
+  // images: {
+  //   loader: "akamai",
+  //   path: "/",
+  // },
+  // output: "export", // 정적 HTML 파일로 변환
 };
 
 module.exports = nextConfig;
